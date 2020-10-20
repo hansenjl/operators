b8c6498942831da703c573be3679298b0a9b0811
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-20 09:00:51 -0700

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,32 @@
+require 'pry'
 def unsafe?(speed)
+    # if speed > 60 || speed < 40 
+    #    true 
+    # else 
+    #     false
+    # end  
+    #return true if the speed is less than 40
 
+    # if speed <= 60 && speed >= 40
+    #     false 
+    #  else 
+    #      true
+    #  end 
+
+    # condition ?  do this if true   :  do this if false 
+    #  speed <= 60 && speed >= 40 ? false : true 
+     speed > 60 || speed < 40 
 end
 
 
 
 def not_safe?(speed)
+     # condition ?  do this if true   :  do this if false 
+     speed > 60 || speed < 40 ? true : false 
+
+    # return true if speed > 60 || speed < 40
+    # false
+    #  # what we want to return  if  condition
 	
 end
 	
